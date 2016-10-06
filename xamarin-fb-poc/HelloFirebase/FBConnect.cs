afb2af4ae6006cfea4cb323eba7376a5a7feb0fc
sharma-shweta <shweta.8448@gmail.com>
2016-10-06 22:35:42 +0800

    Added basic google sign in/out and data posting to the Xamarin FB POC

---
+++ xamarin-fb-poc/HelloFirebase/FBConnect.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.OS;
+using Android.Util;
+using Firebase;
+using Firebase.Database;
+using Android.Widget;
+using System;
+
+namespace HelloFirebase
+{
+	public class ChildEventListener : IChildEventListener
+	{
+		private const string Tag = "FBCRUDTest";
+		public IntPtr Handle
+		{
+			get
+			{
+				return (System.IntPtr)0;
+			}
+		}
+
+		public void Dispose()
+		{
+			Log.Debug(Tag, "Database Dispose");
+		}
+
+		public void OnCancelled(DatabaseError error)
+		{
+			Log.Debug(Tag, "Database Error: " + error.Message);
+		}
+
+		public void OnChildAdded(DataSnapshot snapshot, string previousChildName)
+		{
+			Log.Debug(Tag, "OnChildAdded: " + snapshot.Value + " " + previousChildName);
+		}
+
+		public void OnChildChanged(DataSnapshot snapshot, string previousChildName)
+		{
+			Log.Debug(Tag, "OnChildChanged: " + snapshot.Value + " " + previousChildName);
+		}
+
+		public void OnChildMoved(DataSnapshot snapshot, string previousChildName)
+		{
+			Log.Debug(Tag, "OnChildMoved: " + snapshot.Value + " " + previousChildName);
+		}
+
+		public void OnChildRemoved(DataSnapshot snapshot)
+		{
+			Log.Debug(Tag, "OnChildRemoved: " + snapshot.Value);
+		}
+	}
+
+	[Activity(Label = "CRUD Test", MainLauncher = false)]
+	public class DatabaseTest : Activity, IValueEventListener
+	{
+		private const string Tag = "FBCRUDTest";
+		private DatabaseReference mDatabase;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.Main);
+			// Button button = FindViewById<Button>(Resource.Id.myButton);
+
+			// Setup our firebase options then init
+			FirebaseOptions o = new FirebaseOptions.Builder()
+			    .SetApiKey(GetString(Resource.String.ApiKey))
+				.SetApplicationId(GetString(Resource.String.ApplicationId))
+			    .SetDatabaseUrl(GetString(Resource.String.DatabaseUrl))
+				.Build();
+			FirebaseApp fa = FirebaseApp.InitializeApp(this, o, Application.PackageName);
+
+			// Get a database reference
+			var db = FirebaseDatabase.GetInstance(fa);
+			mDatabase = db.GetReference("shwt-test");
+			mDatabase.AddListenerForSingleValueEvent(this);
+
+			//ChildEventListener c = new ChildEventListener();
+			//mDatabase.AddChildEventListener(new Child() { });
+
+			//mDatabase.SetValue("Hello, Shweta!");
+			mDatabase.Child("user").Child("1").SetValue("Shweta");
+		}
+
+		private void SubmitPost()
+		{
+			// string userId = GetUid();
+			mDatabase.AddListenerForSingleValueEvent(this);
+		}
+
+		public void OnDataChange(DataSnapshot dataSnapshot)
+		{
+			Log.Debug(Tag, "Database Change: " + dataSnapshot.Value);
+		}
+
+		public void OnCancelled(DatabaseError databaseError)
+		{
+			Log.Debug(Tag, "getUser:onCancelled", databaseError.ToException());
+		}
+	}
+}
---
+++ xamarin-fb-poc/HelloFirebase/MainActivity.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.OS;
+using Android.Util;
+using Firebase.Xamarin;
+using Firebase.Xamarin.Database;
+
+		
+namespace HelloFirebase
+{
+	[Activity(Label = "HelloFirebase", Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			// base.OnCreate(savedInstanceState);
+			// SetContentView(Resource.Layout.Main);
+
+			//FirebaseOptions o = new FirebaseOptions.Builder()
+			//	.SetApiKey(GetString(Resource.String.ApiKey))
+			//	.SetApplicationId(GetString(Resource.String.ApplicationId))
+			//	.SetDatabaseUrl(GetString(Resource.String.DatabaseUrl))
+			//	.Build();
+			
+			//var firebase = new FirebaseClient("https://hellofb-b005f.firebaseio.com/");
+			//var item = firebase
+			//		  .Child("work")
+			//		  //.WithAuth("<Authentication Token>") // <-- Add Auth token if required. Auth instructions further down in readme.
+			//		  .PostAsync(new YourObject());
+								
+			/*
+			 * var config = {
+				    apiKey: "AIzaSyBlP8nGeKUpoKCVL7-IPjSdQe1hqnZ-3b0",
+				    authDomain: "hellofb-b005f.firebaseapp.com",
+				    databaseURL: "https://hellofb-b005f.firebaseio.com",
+				    storageBucket: "hellofb-b005f.appspot.com",
+				    messagingSenderId: "79941882991"
+				  };
+			 */
+			System.Console.WriteLine("HELLOOO");
+			//var firebase = new Firebase.Xamarin.Database.FirebaseClient("https://hellofb-b005f.firebaseio.com/");
+			//var items = firebase.Child("work").AsObservable<Work>().Subscribe(d => System.Console.Write(d.Key));
+			////.WithAuth("<Authentication Token>") // <-- Add Auth token if required. Auth instructions further down in readme.
+			//.LimitToFirst(2)
+			//.OnceAsync<YourObject>();
+			//items.O
+			//foreach (var item in items)
+			//{
+			//	System.Console.WriteLine($"{item.Key} name is {item.Object.Name}");
+			//}
+
+			//foreach (var item in items)
+			//{
+			//	System.Console.WriteLine($"{item.Key} name is {item.Object.Name}");
+			//}
+
+			//base.OnCreate(savedInstanceState);
+
+			//// Set our view from the "main" layout resource
+			//SetContentView(Resource.Layout.Main);
+
+			//// Get our button from the layout resource,
+			//// and attach an event to it
+			//Button button = FindViewById<Button>(Resource.Id.myButton);
+
+			//button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+		}
+	}
+}
+
