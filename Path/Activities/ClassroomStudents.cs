0e9b525e92318b89f1b8f25a9a1987bb9bb9727a
RALAPALLI SESHADRI NAIDU MADHAN BABU <MADHANBABU@OUTLOOK.COM>
2016-11-03 21:20:33 +0800

    Updated main launcher

--- Path/Activities/ClassroomStudents.cs
+++ Path/Activities/ClassroomStudents.cs
@@ -11,7 +11,7 @@ using System.Linq;
 
 namespace Path
 {
-    [Activity(Label = "ClassroomStudents", Theme = "@style/MatLightNoActionBar", MainLauncher = true)]
+    [Activity(Label = "ClassroomStudents", Theme = "@style/MatLightNoActionBar", MainLauncher = false)]
     public class ClassroomStudents : Activity
 	{
         StudentsViewModel _model;
